8dbe900ece065d888f33033179e0bf78f3a6c77a
olegchitaoleg <jaguar75oleg@gmail.com>
2024-11-10 19:33:19 +0900

    Add files via upload
    https://github.com/olegchitaoleg/mudule_1_5/upload

---
+++ 'module_1_5'.py
@@ -0,0 +1,9 @@
+immutable_var = 7,4,'coconut','a'
+print(immutable_var)
+#immutable_var[0] = 10/Кортэж относится к неизменяемым типам данных.
+#print(immutable_var)/Ошибка,кортэж не поддерживает обращение по элементам.
+mutable_list = (7,4,5,3)
+print(mutable_list)
+mutable_list = (7,4,5,3) + (23,9)
+print(mutable_list)
+
